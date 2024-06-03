97a7a130371f03d3aa0cd56843f15bad60c6b021
ahmadshah49 <ahmadraza.fsd.pk94@gmail.com>
2024-06-03 19:36:41 +0500

    auth

---
+++ middleware.ts
@@ -0,0 +1,15 @@
+import NextAuth from "next-auth";
+
+import authConfig from "./auth.config";
+
+// import { auth } from "@/auth";
+const { auth } = NextAuth(authConfig);
+
+export default auth((req) => {
+  const isLoggedIn = !!req.auth;
+  console.log("ROUTE :", req.nextUrl.pathname);
+  console.log("IS LOGGED IN :", isLoggedIn);
+});
+export const config = {
+  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
+};
