a20c253cb646163c8eeab667af4868c5e1fffd13
ahmadshah49 <ahmadraza.fsd.pk94@gmail.com>
2024-06-04 17:19:39 +0500

    authentication complete

--- middleware.ts
+++ middleware.ts
@@ -2,14 +2,41 @@ import NextAuth from "next-auth";
 
 import authConfig from "./auth.config";
 
-// import { auth } from "@/auth";
+import {
+  DEFAULT_LOGIN_REDIRECT,
+  apiAuthPrefix,
+  authRoutes,
+  publicRoutes,
+} from "@/routes";
+import { NextResponse } from "next/server";
+
 const { auth } = NextAuth(authConfig);
 
 export default auth((req) => {
+  const { nextUrl } = req;
   const isLoggedIn = !!req.auth;
-  console.log("ROUTE :", req.nextUrl.pathname);
-  console.log("IS LOGGED IN :", isLoggedIn);
+
+  const isApiAuthRoute = nextUrl.pathname.startsWith(apiAuthPrefix);
+  const isPublicRoute = publicRoutes.includes(nextUrl.pathname);
+  const isAuthRoute = authRoutes.includes(nextUrl.pathname);
+  if (isApiAuthRoute) {
+    return NextResponse.next();
+  }
+
+  if (isAuthRoute) {
+    if (isLoggedIn) {
+      return Response.redirect(new URL(DEFAULT_LOGIN_REDIRECT, nextUrl));
+    }
+    return NextResponse.next();
+  }
+
+  if (!isLoggedIn && !isPublicRoute) {
+    return Response.redirect(new URL("auth/login", nextUrl));
+  }
+
+  return NextResponse.next();
 });
+
 export const config = {
   matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
 };
